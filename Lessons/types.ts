a54880eef19a2e0ce4c10665163c94e2af8f430e
Marco.Polenta <polentamarco.dev@gmail.com>
2024-02-29 12:17:15 +0100

    Adding new examples for interface, classes and types

--- Lessons/types.ts
+++ Lessons/types.ts
@@ -1,4 +1,44 @@
-function formatMessage(countryName, population = 0) {
-    return `The population of ${countryName} is ${population}` 
+function formatMessage(countryName:string, population:number = 0) {
+    return `The population of ${countryName} is ${population}`;
 }
 
+let country:string = "Thailand";
+
+let population:number = 69_950_850;
+
+const message:string = formatMessage(country, population);
+
+console.log(message);
+
+
+// Define an interface for a Person
+interface IPerson {
+    firstName:string;
+    lastName:string;
+    
+    getFullName():string;
+  }
+  
+  // Define a class representing a Person implementing the IPerson interface
+  class Person implements IPerson {
+    // Properties
+    firstName:string;
+    lastName:string;
+    
+    // Constructor
+    constructor(firstName:string, lastName:string) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+    
+    // Method to get the full name
+    getFullName():string {
+      return `${this.firstName} ${this.lastName}`;
+    }
+  }
+  
+  // Create an instance of Person
+  const person1:IPerson = new Person('John', 'Doe');
+  
+  // Print out the full name
+  console.log(person1.getFullName()); // Output: John Doe
