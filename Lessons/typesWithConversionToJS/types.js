b920ad18c478d692a3cab5f2a497edf9883a216e
Marco.Polenta <polentamarco.dev@gmail.com>
2024-02-29 15:51:51 +0100

    added more depth to the document

--- Lessons/typesWithConversionToJS/types.js
+++ Lessons/typesWithConversionToJS/types.js
@@ -1,16 +1,32 @@
+// Types:
+// Strings:
+var city = "New York";
+// Numbers:
+var height = 1.82;
+// Boolean:
+var itRains = true;
+console.log(city, height, itRains);
+console.log("----------------------------------------------------------");
+// Other Types: any types
+// null:
+var currencies = null; //they can be also set up as types
+// undefined:
+var languages = undefined; //they can be also set up as types
+// any:
+var object = {};
+console.log(currencies, languages, object);
+console.log("----------------------------------------------------------");
+// First application of types:
 function formatMessage(countryName, population) {
     if (population === void 0) { population = 0; }
     return "The population of ".concat(countryName, " is ").concat(population);
 }
-
 var country = "Thailand";
 var population = 69950850;
 var message = formatMessage(country, population);
-
+//                     ^?
 console.log(message);
-
 console.log("----------------------------------------------------------");
-
 // Define a class representing a Person implementing the IPerson interface
 var Person = /** @class */ (function () {
     // Constructor
@@ -18,26 +34,41 @@ var Person = /** @class */ (function () {
         this.firstName = firstName;
         this.lastName = lastName;
     }
-
     // Method to get the full name
     Person.prototype.getFullName = function () {
         return "".concat(this.firstName, " ").concat(this.lastName);
     };
     return Person;
 }());
-
 // Create an instance of Person
 var person1 = new Person('John', 'Doe');
-
 // Print out the full name
 console.log(person1.getFullName()); // Output: John Doe
-
 // Create a new object to hold additional details for person1
 var personDetails = {
     age: 30,
     email: 'john.doe@example.com'
 };
-
 // Print out additional details
 console.log("Age: ".concat(personDetails.age));
 console.log("Email: ".concat(personDetails.email));
+console.log("----------------------------------------------------------");
+// Adding an array:
+var poker = ["10F", "10H", "10S", "10D"];
+console.log(poker);
+console.log("----------------------------------------------------------");
+// Adding a function with optional parameter:
+var salary = function (person2, sum, currency) {
+    var sentence = "".concat(person2.getFullName(), " salary is ").concat(sum);
+    if (typeof currency === "string") {
+        sentence += " ".concat(currency);
+    }
+    return sentence;
+};
+var person2 = new Person('Jake', 'Negan');
+var description1 = salary(person2, 1335, "€");
+var description2 = salary(person2, 1335);
+console.log(description1);
+console.log("-------------");
+console.log(description2);
+console.log("----------------------------------------------------------");
--- Lessons/typesWithConversionToJS/types.ts
+++ Lessons/typesWithConversionToJS/types.ts
@@ -1,4 +1,43 @@
-function formatMessage(countryName:string, population:number = 0) {
+// Types:
+
+// Strings:
+
+let city:string = "New York";
+
+// Numbers:
+
+let height:number = 1.82;
+
+// Boolean:
+
+let itRains:boolean = true;
+
+console.log(city, height, itRains);
+
+console.log("----------------------------------------------------------")
+
+// Other Types: any types
+
+// null:
+
+let currencies = null;    //they can be also set up as types
+
+// undefined:
+
+let languages = undefined;    //they can be also set up as types
+
+// any:
+
+let object:any = {};
+
+console.log(currencies, languages, object);
+
+console.log("----------------------------------------------------------")
+
+
+// First application of types:
+
+function formatMessage(countryName:string, population:number = 0):string {
     return `The population of ${countryName} is ${population}`;
 }
 
@@ -7,10 +46,13 @@ let country:string = "Thailand";
 let population:number = 69_950_850;
 
 const message:string = formatMessage(country, population);
+//                     ^?
 
 console.log(message);
 
 console.log("----------------------------------------------------------")
+
+// Going deeper in the builtins:
 
 // Define an interface for a Person
 interface IPerson {
@@ -38,7 +80,7 @@ class Person implements IPerson {
 }
 
 // Create an instance of Person
-const person1: IPerson = new Person('John', 'Doe');
+const person1:IPerson = new Person('John', 'Doe');
 
 // Print out the full name
 console.log(person1.getFullName()); // Output: John Doe
@@ -58,3 +100,37 @@ const personDetails:IPersonDetails = {
 // Print out additional details
 console.log(`Age: ${personDetails.age}`);
 console.log(`Email: ${personDetails.email}`);
+
+console.log("----------------------------------------------------------")
+
+
+// Adding an array:
+
+let poker:string[] = ["10F", "10H", "10S", "10D"]
+
+console.log(poker)
+
+console.log("----------------------------------------------------------")
+
+
+// Adding a function with optional parameter:
+
+const salary = (person2:Person, sum:number, currency?:string): void => {
+  let sentence = `${person2.getFullName()} salary is ${sum}`
+
+  if (typeof currency === "string"){
+    sentence += ` ${currency}`;
+  }
+
+  return sentence;
+}
+
+const person2:IPerson = new Person('Jake', 'Negan');
+
+let description1 = salary(person2, 1335, "€");
+let description2 = salary(person2, 1335);
+
+console.log(description1);
+console.log("-------------")
+console.log(description2);
+console.log("----------------------------------------------------------")
